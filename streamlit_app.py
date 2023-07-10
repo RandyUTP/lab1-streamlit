fb372863f52f91cb2635a27ec7c64399d638147e
RandyUTP <117698791+RandyUTP@users.noreply.github.com>
2023-07-10 11:08:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -217,7 +217,7 @@ def show_page3():
     #En este ejemplo, estamos contando el número de casos de anemia por microred y creando un gráfico circular que muestra la distribución de casos entre las microredes
     # Obtener el conteo de casos por microred
     st.write("gráfico circular número de casos de anemia por microred")
-    casos_por_microred = data['MICRORED'].value_counts()
+    casos_por_microred = dataset['MICRORED'].value_counts()
     # Crear el gráfico circular
     plt.figure(figsize=(8, 8))
     plt.pie(casos_por_microred, labels=casos_por_microred.index, autopct='%1.1f%%')
@@ -225,6 +225,87 @@ def show_page3():
     plt.axis('equal')
     st.set_option('deprecation.showPyplotGlobalUse', False)
     st.pyplot(plt.show())
+
+    #En este ejemplo, agrupamos los datos por provincia y calculamos el número total de casos de anemia y el promedio de edad de los casos en cada provincia. Luego, ordenamos las provincias por el número total de casos en orden descendente.
+    #Creamos un gráfico de barras agrupadas donde las barras representan el número total de casos de anemia por provincia. Además, agregamos una línea punteada que representa el promedio de edad de los casos en cada provincia.
+    #El eje y izquierdo corresponde al número de casos, y el eje y derecho corresponde al promedio de edad. Utilizamos colores diferentes para cada eje y ajustamos los parámetros para mostrar las etiquetas de las provincias de forma adecuada. """
+    st.write("Número total de casos y promedio de edad por provincia")
+    # Agrupar los datos por provincia y calcular el número total de casos y el promedio de edad
+    datos_provincia = dataset.groupby('PROVINCIA').agg({'CASOS': 'sum', 'EDAD': 'mean'})
+    # Ordenar las provincias por el número total de casos en orden descendente
+    datos_provincia = datos_provincia.sort_values(by='CASOS', ascending=False)
+    # Crear el gráfico de barras agrupadas
+    fig, ax1 = plt.subplots(figsize=(20, 6))
+    # Barra para el número total de casos
+    ax1.bar(datos_provincia.index, datos_provincia['CASOS'], color='tab:blue')
+    ax1.set_ylabel('Número de casos', color='tab:blue')
+    ax1.tick_params(axis='y', labelcolor='tab:blue')
+    ax1.set_xlabel('Provincia')
+    # Promedio de edad como línea punteada
+    ax2 = ax1.twinx()
+    ax2.plot(datos_provincia.index, datos_provincia['EDAD'], color='tab:red', linestyle='--')
+    ax2.set_ylabel('Promedio de edad', color='tab:red')
+    ax2.tick_params(axis='y', labelcolor='tab:red')
+    # Ajustar el espaciado de las etiquetas de las provincias
+    plt.xticks(rotation=45, ha='right')
+    plt.title('Número total de casos y promedio de edad por provincia')
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    st.pyplot(plt.show())
+
+
+    #""" En este ejemplo, seleccionamos las provincias que queremos comparar y filtramos los datos del DataFrame original para incluir solo esas provincias. Luego, agrupamos los datos por año y provincia, y calculamos el total de casos de anemia por año para cada provincia.
+    #Después, creamos un gráfico de líneas múltiples donde cada línea representa la evolución de casos de anemia en una provincia específica a lo largo de los años. """
+    st.write("Evolución de casos de anemia por provincia")
+    # Seleccionar las provincias a comparar
+    provincias = ['CUSCO', 'CALCA', 'ANTA']
+    # Filtrar los datos para las provincias seleccionadas
+    data_provincias = dataset[dataset['PROVINCIA'].isin(provincias)]
+    # Agrupar los datos por año y provincia y calcular el total de casos por año
+    casos_por_anio_provincia = data_provincias.groupby(['ANIO', 'PROVINCIA'])['CASOS'].sum().unstack()
+    # Crear el gráfico de líneas múltiples
+    plt.figure(figsize=(10, 6))
+    for provincia in provincias:
+    plt.plot(casos_por_anio_provincia.index, casos_por_anio_provincia[provincia], marker='o', label=provincia)
+    plt.title('Evolución de casos de anemia por provincia')
+    plt.xlabel('Año') 
+    plt.ylabel('Total de casos')
+    plt.legend()
+    plt.grid(True)
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    st.pyplot(plt.show())  
+
+
+    #En este ejemplo, estamos utilizando los datos de edad, casos normales y casos totales para crear un gráfico de dispersión tridimensional. Cada punto en el gráfico representa una combinación de edad, casos normales y casos totales."""
+    st.write("Relación entre edad, casos normales y casos totales")
+    # Obtener los valores de edad, casos normales y casos totales
+    edad = dataset['EDAD']
+    casos_normales = dataset['NORMAL']
+    casos_totales = dataset['TOTAL']
+    # Crear el gráfico de dispersión tridimensional
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(edad, casos_normales, casos_totales)
+    ax.set_title('Relación entre edad, casos normales y casos totales')
+    ax.set_xlabel('Edad')
+    ax.set_ylabel('Casos Normales')
+    ax.set_zlabel('Casos Totales')
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    st.pyplot(plt.show()) 
+        
+    #En este ejemplo, estamos utilizando los datos de edades para crear un gráfico de caja y bigotes.
+    #El gráfico muestra la distribución de las edades mediante una caja que representa el rango intercuartil (IQR), los bigotes que muestran el rango completo de los datos y los puntos que pueden indicar valores atípicos. """
+    st.write("Distribución de edades")
+    # Obtener los valores de edad
+    edades = dataset['EDAD']
+    # Crear el gráfico de caja y bigotes
+    plt.figure(figsize=(8, 6))
+    plt.boxplot(edades, vert=False)
+    plt.title('Distribución de edades')
+    plt.xlabel('Edad')
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    st.pyplot(plt.show()) 
+
+
 
 
 
