e9971a0ff1f2de9d55d7694fab112eaab5e743d8
RandyUTP <117698791+RandyUTP@users.noreply.github.com>
2023-07-10 01:58:46 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,4 @@ from PIL import Image
 @st.cache
 def load_dataset():
     dataset = pd.read_csv('Casos_Anemia_Region_Cusco_2010_2020_Cusco.csv', encoding='latin-1' , sep=';')
-return dataset
+    return dataset
