aa1552b14d065bd31e3de826063aa857c91358d2
RandyUTP <117698791+RandyUTP@users.noreply.github.com>
2023-07-10 08:38:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,9 @@ def load_dataset():
     dataset = pd.read_csv('Casos_Anemia_Region_Cusco_2010_2020_Cusco.csv', encoding='latin-1' , sep=';')
     return dataset
 
+
+
+plt.figure(figsize=(16,6))
+plt.bar(data_nn['PROVINCIA'].unique(),data['PROVINCIA'].value_counts())
+plt.title('G')
+plt.show()
