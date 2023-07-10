f4052a87b04d76c7a2c01c41879f60d4a26db028
RandyUTP <117698791+RandyUTP@users.noreply.github.com>
2023-07-10 08:53:43 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ from PIL import Image
 
 
 
+# Cargar el dataset y almacenarlo en caché
 @st.cache
 def load_dataset():
     dataset = pd.read_csv('Casos_Anemia_Region_Cusco_2010_2020_Cusco.csv', encoding='latin-1' , sep=';')
@@ -31,4 +32,98 @@ def show_home():
     c2.markdown("## Modelos predictivos con aprendizaje automático")
     c2.markdown("#### Integrantes:")
     c2.write("- Rivera Cumpa Pyerina")
+
+def show_page1():
+    st.title("Carga de datos del dataset")
+    st.write("Através de la librería pandas se realiza la carga de datos de nuestro dataset")
+    
+    # Crear un contenedor con un estilo de fondo personalizado
+    contenedor = st.container()
+    
+    # Cargar el dataset
+    st.markdown("### Importar librería")
+    st.write("import pandas as pd")
+    
+    st.markdown("### Cargar datos")
+    st.write("""@st.cache
+    def load_dataset():
+    dataset = pd.read_csv('Casos_Anemia_Region_Cusco_2010_2020_Cusco.csv', encoding='latin-1' , sep=';')
+    return dataset""")
+
+    st.markdown("### Mostrar datos")
+    dataset = load_dataset()
+    # Mostrar la tabla con los datos
+    st.write(dataset)
+
+def show_page2():
+    st.title("Describir datos")
+    st.write("Importante para determinar problemas de calidad de datos")
+    dataset = load_dataset()
+    
+    # Mostrar descripción de los datos
+    st.markdown("### Descripción del dataset:")
+    st.write(dataset.describe())
+    st.write("De los resultados obtenemos que: El rango de edades se encuentra entre 0 y 59 años, también que el promedio es de 26,65")
+    
+    # Obtener el contenido de los datos de las subcategorías de la categoría "provincia"
+    subcategorias = dataset["PROVINCIA"].value_counts()
+    st.markdown("### Conteo por categoría:")
+    st.write("Categoría provincia: ")
+    st.write(subcategorias)
+    st.write("Los resultados muestran que la moda de la categoría provincia es La Convención")
+    
+    # Obtener el contenido de los datos de las subcategorías de la categoría "distrito"
+    subcategoriaD = dataset["DISTRITO"].value_counts()
+    st.write("Categoría distrito: ")
+    st.write(subcategoriaD)
+    st.write("Los resultados muestran que la moda de la categoría distrito es Echarate")
+
+    st.markdown("### Promedio por cartegoría:")
+    # Calcular el promedio de edad por provincia
+    promedio_edad_por_provincia = dataset.groupby("PROVINCIA")["EDAD"].mean()
+    # Mostrar el promedio de edad por provincia
+    st.write("Promedio de edad por provincia:")
+    st.write(promedio_edad_por_provincia)
+    st.write("De los resultados obtenidos se puede observar que los promedios de edad por provincia se encuentran en un rango de 20 y 30 años")
+
+def show_page3():
+    st.title("Visualizar datos: ")
+    dataset = load_dataset()
+
+    st.write("Los gráficos presentados a continuación tienen el objetivo de mostrar pictoricamente los datos que se tienen en el dataset para una mejor comprensión")
+     # Calcular el promedio de edad por provincia
+    st.markdown("### Gráficos de barras:")
+    st.write("Gráfico de barras del promedio de edad por provincia de los pacientes con anemia en Cusco:")
+    promedio_EP = dataset.groupby('PROVINCIA')['EDAD'].mean()
+    st.bar_chart(promedio_EP)
+    
+    # Conteo de los datos por provincia
+    st.write("Gráfico de barras del número de casos de anemia por provincia:")
+    conteo = dataset["PROVINCIA"].value_counts()
+    st.bar_chart(conteo)
+
+    st.write("Gráfico de barras del promedio de casos de anemia por año y provincia")
+    # Seleccionar las provincias a comparar
+    provincias = ['ESPINAR', 'CANAS', 'PARURO']
+    # Filtrar los datos para las provincias seleccionadas
+    data_provincias = dataset[dataset['PROVINCIA'].isin(provincias)]
+    # Calcular los promedios de casos de anemia por año y provincia
+    promedios_por_anio_provincia = data_provincias.groupby(['ANIO', 'PROVINCIA'])['CASOS'].mean().unstack()
+    # Mostrar el gráfico de barras
+    st.bar_chart(promedios_por_anio_provincia)
+
+    st.markdown("### Gráficos de líneas:")
+    st.write("Gráfico de la evolución de casos de anemia por provincia")
+    # Seleccionar las provincias a comparar
+    provincias = ['CUSCO', 'CALCA', 'ANTA']
+    # Filtrar los datos para las provincias seleccionadas
+    data_provincias = dataset[dataset['PROVINCIA'].isin(provincias)]
+    # Agrupar los datos por año y provincia y calcular el total de casos por año
+    casos_por_anio_provincia = data_provincias.groupby(['ANIO', 'PROVINCIA'])['CASOS'].sum().unstack()
+    # Mostrar el gráfico de líneas múltiples en un solo gráfico
+    for provincia in provincias:
+        st.line_chart(casos_por_anio_provincia[provincia])
+
+if __name__ == "__main__":
+    main()
     
