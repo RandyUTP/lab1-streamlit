83ed5936d45e2136267a574873b7439ace08504f
RandyUTP <117698791+RandyUTP@users.noreply.github.com>
2023-07-10 09:00:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -123,6 +123,9 @@ def show_page3():
     # Mostrar el gráfico de líneas múltiples en un solo gráfico
     for provincia in provincias:
         st.line_chart(casos_por_anio_provincia[provincia])
+    #mostrar datos 
+      data_nn =dataset.dropna(subset=['PROVINCIA', 'DISTRITO'])
+      st.write(data_nn.isnull().sum())
 
 if __name__ == "__main__":
     main()
