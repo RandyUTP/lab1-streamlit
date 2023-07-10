b433701722bcca43c0b923804c967771eb9ac2d9
RandyUTP <117698791+RandyUTP@users.noreply.github.com>
2023-07-10 01:52:08 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ from PIL import Image
 image = Image.open('hidra.png')
 st.image(image,use_column_width=True)
 
-data=pd.read_csv('/Casos_Anemia_Region_Cusco_2010_2020_Cusco.csv' , encoding='latin-1', sep=';')
+data=pd.read_csv('Casos_Anemia_Region_Cusco_2010_2020_Cusco.csv' , encoding='latin-1', sep=';')
 data.head(5)
