8d5f337f61d2799c6080ddb0ec666746ba7f60ea
RandyUTP <117698791+RandyUTP@users.noreply.github.com>
2023-07-10 08:49:35 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,19 @@ from PIL import Image
 def load_dataset():
     dataset = pd.read_csv('Casos_Anemia_Region_Cusco_2010_2020_Cusco.csv', encoding='latin-1' , sep=';')
     return dataset
+def main():
+    # Configurar la barra de navegación
+    st.sidebar.title("Navegación")
+    pages = {
+        "Inicio": show_home,
+        "Cargar": show_page1,
+        "Describir": show_page2,
+        "Visualizar": show_page3
+    }
+    page = st.sidebar.selectbox("Ir a", tuple(pages.keys()))
 
+    # Mostrar la página seleccionada
+    pages[page]()
 def  carga():
     dataset = load_dataset()
     data_nn =dataset.dropna(subset=['PROVINCIA', 'DISTRITO'])
