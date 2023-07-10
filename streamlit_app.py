7d7fabe2d8b9f003c4ddbccb304d12b5be55baf5
RandyUTP <117698791+RandyUTP@users.noreply.github.com>
2023-07-10 01:55:51 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,5 @@ st.image(image,use_column_width=True)
 
 @st.cache
 def load_dataset():
-data=pd.read_csv('Casos_Anemia_Region_Cusco_2010_2020_Cusco.csv' , encoding='latin-1', sep=';')
-data.head(5)
-return data
+    dataset = pd.read_csv('Casos_Anemia_Region_Cusco_2010_2020_Cusco.csv', encoding='latin-1' , sep=';')
+return dataset
