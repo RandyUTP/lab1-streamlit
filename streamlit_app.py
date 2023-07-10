b972b12253be6f4ee103b8a51787a24605348cae
RandyUTP <117698791+RandyUTP@users.noreply.github.com>
2023-07-10 09:18:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -130,7 +130,7 @@ def show_page3():
     plt.figure(figsize=(16,6))
     plt.bar(data_nn['PROVINCIA'].unique(),dataset['PROVINCIA'].value_counts())
     plt.title('G')
-    st.write(plt.show())
+    plt.show()
 
 if __name__ == "__main__":
     main()
